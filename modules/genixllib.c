2922b0857fa9c0e7739b6b201b5230eff2296736
broli <carlos.ferrabone@gmail.com>
2009-11-25 23:11:26 -0300

    added return codes

--- modules/genixllib.c
+++ modules/genixllib.c
@@ -41,73 +41,92 @@ menu_items_t printmenu(menu_items_t menu,mxml_node_t *tree)
 	if ( menu == MENU_MAIN )
 	{
 		/*TODO make main menu*/
+		return MENU_MAIN;
 	}
 	else if ( menu == MENU_SOURCE )
 	{
 		/*TODO make source menu*/
+		return MENU_SOURCE;
 	}
 	else if ( menu == MENU_DESTINATION )
 	{
 		/*TODO make destination menu*/
+		return MENU_DESTINATION;
 	}
 	else if ( menu == MENU_LINK )
 	{
 		/*TODO make link menu */
+		return MENU_LINK;
 	}
 	else if ( menu == MENU_IMAGE )
 	{
 		/*TODO make image menu*/
+		return MENU_IMAGE;
 	}
 	else if ( menu == MENU_COLOR )
 	{
 		/*TODO make color menu*/
+		return MENU_COLOR;
 	}
 	else if ( menu == MENU_MARGIN )
 	{
 		/*TODO make margin menu*/
+		return MENU_MARGIN;
 	}
 	else if ( menu == MENU_SECURITY )
 	{
 		/*TODO make security menu*/
+		return MENU_SECURITY;
 	}
 	else if ( menu == MENU_TEXT )
 	{
 		/*TODO make text menu*/
+		return MENU_TEXT;
 	}
 	else if ( menu == MENU_DOCUMENT )
 	{
 		/*TODO make document menu */
+		return MENU_DOCUMENT;
 	}
 	else if ( menu == MENU_BOOKMARKS )
 	{
 		/*TODO Make bookmarks menu*/
+		return MENU_BOOKMARKS;
 	}
 	else if ( menu == MENU_LASTCONVERSION )
 	{
 		/*TODO read the ixl docs and find out if 
 		 * this can/should be modified by other
 		 * programs (other than iSilox that is*/
+		/*For now we return error to catch this*/
+		return MENU_ERROR;
 	}
 	else if ( menu == MENU_MESSAGES )
 	{
 		/*TODO make a messages menu*/
+		return MENU_MESSAGES;
 	}
 	else if ( menu == MENU_COOKIES )
 	{
 		/*TODO make a cookies menu
 		 * (not all options) */
+		return MENU_COOKIES;
 	}
 	else if ( menu == MENU_SCHEDULING )
 	{
 		/* I have no idea what this does
 		 * and no intention of suporting it for now*/
+		return MENU_ERROR;
 	}
 	else if ( menu == MENU_TARGETS )
 	{
 		/* I have no idea what this does
 		 * and no intention of suporting it for now*/
+		return MENU_ERROR;
 
 	}
+
+	return MENU_ERROR;
 } /* menu_items_t printmenu(menu_items_t menu) */
 
 
