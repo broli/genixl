a159ccedaee9e19b86e5a6fd21976a493c46d43c
broli <carlos.ferrabone@gmail.com>
2010-01-03 16:46:58 -0300

    Look and feel
    Deleted all the "make it pretty" new lines, because it actually looks
    better wihtout empty lines all over
    
    also fixed some numeration problems

--- modules/genixllib.c
+++ modules/genixllib.c
@@ -268,15 +268,15 @@ void printmenuMain(void)
 {
 	system("clear");
 	printf("Main\n----\n");
-	printf( "1)   Source->\n"
-		"2)   Destination->\n"
-		"3)   LinkOptions->\n"
-		"4)   ImageOptions->\n"
-		"5)   TableOptions->\n"
-		"6)   ColorOptions->\n"
-		"7)   MarginOptions->\n"
-		"8)   SecurityOptions->\n"
-		"9)   TextOptions->\n"
+	printf( "01)  Source->\n"
+		"02)  Destination->\n"
+		"03)  LinkOptions->\n"
+		"04)  ImageOptions->\n"
+		"05)  TableOptions->\n"
+		"06)  ColorOptions->\n"
+		"07)  MarginOptions->\n"
+		"08)  SecurityOptions->\n"
+		"09)  TextOptions->\n"
 		"10)  DocumentOptions->\n"
 		"11)  Bookmarks->\n"
 		"12)  LastConversion->\n"
@@ -302,35 +302,32 @@ menu_items_t printmenuSource(mxml_node_t *tree)
 	if ( SourceNode != NULL )
 	{
 		/*search and handwalk */
-		printf("1) Sources:\n");
+		printf("01) Sources:\n");
 		/*print all the Paths in the sources node*/
 		PrintPath(SourceNode,"Sources");
 
 		/*Now, lets search the User name********************************************/
 
-		printf("\n"); /*make some room*/
-		printf("2) UserName: %s\n",getElemData(SourceNode,"UserName") );
+		printf("02) UserName: %s\n",getElemData(SourceNode,"UserName") );
 
 		/*Now, lets search the Password*******************************************/
 
-		printf("\n"); /*make some room*/
-		printf("3) Password: %s\n",getElemData(SourceNode,"Password") );
+		printf("03) Password: %s\n",getElemData(SourceNode,"Password") );
 
 		/*Now, lets search the CharSet********************************************/
 
-		printf("\n"); /*make some room*/
-		printf("4) CharSet: %s\n\n",getElemData(SourceNode,"CharSet") );
+		printf("04) CharSet: %s\n\n",getElemData(SourceNode,"CharSet") );
 
 	}
 	else
 	{
 		/*No source node*/
 		/*print empty */
-		printf("1) Sources:\n"
+		printf("01) Sources:\n"
 				"*\t<n/a>\n\n"
-			"2) UserName: <n/a>\n\n"
-			"3) Password: <n/a>\n\n"
-			"4) CharSet: <n/a>\n"
+			"02) UserName: <n/a>\n\n"
+			"03) Password: <n/a>\n\n"
+			"04) CharSet: <n/a>\n"
 			"\n");
 	}
 	return MENU_SOURCE;
@@ -351,24 +348,21 @@ menu_items_t printmenuDestination(mxml_node_t *tree)
 	/*the search might return null*/
 	if ( Destination != NULL )
 	{
-		printf("1) Title: %s\n",getElemData(Destination,"Title") );
+		printf("01) Title: %s\n",getElemData(Destination,"Title") );
 		
 		/*Now, lets search the hotsync*********************************************/
 		
-		printf("\n"); /*make it pretty*/
-		printf("2) HotSync:\n");
+		printf("02) HotSync:\n");
 		PrintPath(Destination,"HotSync");
 
 		/*Now, lets search the ActiveSync ***************************************/
 		
-		printf("\n"); /*make it pretty*/
-		printf("3) ActiveSync:\n");
+		printf("03) ActiveSync:\n");
 		PrintPath(Destination,"ActiveSync");
 
 		/*Now, lets search the Files ***************************************/
 		
-		printf("\n"); /*make it pretty*/
-		printf("3) Files:\n");
+		printf("05) Files:\n");
 		PrintPath(Destination,"Files");
 
 		printf("\n"); /*make it pretty*/
@@ -378,13 +372,13 @@ menu_items_t printmenuDestination(mxml_node_t *tree)
 	else 
 	{
 		/*No destination node, print empty*/
-		printf("1) Title: <n/a>\n\n"
-			"2) HotSync:\n"
-				"\t<n/a>\n\n"
-			"3) ActiveSync:\n"
-				"\t<n/a>\n\n"
-			"4) Files:\n"
-				"\t<n/a>\n\n");
+		printf("01) Title: <n/a>\n"
+			"02) HotSync:\n"
+				"\t<n/a>\n"
+			"03) ActiveSync:\n"
+				"\t<n/a>\n"
+			"04) Files:\n"
+				"\t<n/a>\n");
 	}
 
 	return MENU_DESTINATION;
@@ -405,37 +399,31 @@ menu_items_t printmenuLinkOptions(mxml_node_t *tree)
 	if ( LinkOptions != NULL )
 	{
 		/*Print MaximumDepth*/
-		printf("1) MaximumDepth:\t%s\n",getElemValue(LinkOptions,"MaximumDepth"));
+		printf("01) MaximumDepth:\t\t%s\n",getElemValue(LinkOptions,"MaximumDepth"));
 
 		/*Print FollowOffsite*/
-		printf("\n"); /*make it pretty*/
-		printf("2) FollowOffsite:\t%s\n",getElemValue(LinkOptions,"FollowOffsite") );
+		printf("02) FollowOffsite:\t\t%s\n",getElemValue(LinkOptions,"FollowOffsite") );
 
 		/*Print MaximumOffsiteDepth*/
-		printf("\n"); /*make it pretty*/
-		printf("2) MaximumOffsiteDepth:\t%s\n",getElemValue(LinkOptions,"MaximumOffsiteDepth") );
+		printf("03) MaximumOffsiteDepth:\t%s\n",getElemValue(LinkOptions,"MaximumOffsiteDepth") );
 
 		/*Print SubDirOnly*/
-		printf("\n"); /*make it pretty*/
-		printf("2) SubDirOnly:\t\t%s\n",getElemValue(LinkOptions,"SubDirOnly") );
+		printf("04) SubDirOnly:\t\t\t%s\n",getElemValue(LinkOptions,"SubDirOnly") );
 
 		/*Print UnresolvedDetail*/
-		printf("\n"); /*make it pretty*/
-		printf("2) UnresolvedDetail:\t%s\n",getElemValue(LinkOptions,"UnresolvedDetail") );
+		printf("05) UnresolvedDetail:\t\t%s\n",getElemValue(LinkOptions,"UnresolvedDetail") );
 
 		/*Print Exclude*/
-		printf("\n"); /*make it pretty*/
-		printf("2) Exclude:\n");
+		printf("06) Exclude:\n");
 		PrintPattern(LinkOptions,"Exclude");
 
 		/*Print include*/
-		printf("\n"); /*make it pretty*/
-		printf("2) Include:\n");
+		printf("07) Include:\n");
 		PrintPattern(LinkOptions,"Include");
 
 		/*Print External Docuemnts*/
 		printf("\n"); /*make it pretty*/
-		printf("8) ExternalDocuments ->(not implemented for now)\n\n");
+		printf("08) ExternalDocuments ->(not implemented for now)\n\n");
 
 
 
@@ -443,16 +431,16 @@ menu_items_t printmenuLinkOptions(mxml_node_t *tree)
 	else 
 	{
 		/*No LinkOptions node, print empty*/
-		printf("1) MaximumDepth: \t<n/a>\n\n"
-			"2) FollowOffsite: \t<n/a>\n\n"
-			"3) MaximumOffsiteDepth: <n/a>\n\n"
-			"4) SubDirOnly: \t\t<n/a>\n\n"
-			"5) UnresolvedDetail \t<n/a>\n\n"
-			"6) Exclude:\n"
-				"\t<n/a>\n\n"
-			"7) Include:\n"
-				"\t<n/a>\n\n"
-			"8) ExternalDocuments ->(not implemented for now)\n\n");
+		printf("01) MaximumDepth: \t<n/a>\n"
+			"02) FollowOffsite: \t<n/a>\n"
+			"03) MaximumOffsiteDepth: <n/a>\n"
+			"04) SubDirOnly: \t\t<n/a>\n"
+			"05) UnresolvedDetail \t<n/a>\n"
+			"06) Exclude:\n"
+				"\t<n/a>\n"
+			"07) Include:\n"
+				"\t<n/a>\n"
+			"08) ExternalDocuments ->(not implemented for now)\n");
 	}
 
 	return  MENU_LINK;
@@ -472,63 +460,48 @@ menu_items_t printmenuImageOptions(mxml_node_t *tree)
 	if ( ImageOptions != NULL )
 	{
         	/*Print AltText*/
-		printf("\n"); /*make it pretty*/
 		printf("01) AltText:\t\t%s\n",getElemValue(ImageOptions,"AltText") );
 
 		/*Print Images*/
-		printf("\n"); /*make it pretty*/
 		printf("02) Images:\t\t%s\n",getElemValue(ImageOptions,"Images") );
 
 		/*Print ResizeLargeImages*/
-		printf("\n"); /*make it pretty*/
 		printf("03) ResizeLargeImages:\t%s\n",getElemValue(ImageOptions,"ResizeLargeImages") );
 
 		/*Print MaximumWidth*/
-		printf("\n"); /*make it pretty*/
 		printf("04) MaximumWidth:\t%s\n",getElemValue(ImageOptions,"MaximumWidth") );
 
 		/*Print MaximumHeight*/
-		printf("\n"); /*make it pretty*/
 		printf("05) MaximumHeight:\t%s\n",getElemValue(ImageOptions,"MaximumHeight") );
 
 		/*Print ImproveContrast*/
-		printf("\n"); /*make it pretty*/
 		printf("06) ImproveContrast:\t%s\n",getElemValue(ImageOptions,"ImproveContrast") );
 
 		/*Print Dither*/
-		printf("\n"); /*make it pretty*/
 		printf("07) Dither:\t\t%s\n",getElemValue(ImageOptions,"Dither") );
 
 		/*Print Compress*/
-		printf("\n"); /*make it pretty*/
 		printf("08) Compress:\t\t%s\n",getElemValue(ImageOptions,"Compress") );
 
 		/*Print LossyLevel*/
-		printf("\n"); /*make it pretty*/
 		printf("09) LossyLevel:\t\t%s\n",getElemValue(ImageOptions,"LossyLevel") );
 
 		/*Print LossyQuality*/
-		printf("\n"); /*make it pretty*/
 		printf("10) LossyQuality:\t%s\n",getElemValue(ImageOptions,"LossyQuality") );
 
 		/*Print BitDepth1*/
-		printf("\n"); /*make it pretty*/
 		printf("11) BitDepth1:\t\t%s\n",getElemValue(ImageOptions,"BitDepth1") );
 
 		/*Print BitDepth2*/
-		printf("\n"); /*make it pretty*/
 		printf("12) BitDepth2:\t\t%s\n",getElemValue(ImageOptions,"BitDepth2") );
 
 		/*Print BitDepth4*/
-		printf("\n"); /*make it pretty*/
 		printf("13) BitDepth4:\t\t%s\n",getElemValue(ImageOptions,"BitDepth4") );
 
 		/*Print BitDepth8*/
-		printf("\n"); /*make it pretty*/
 		printf("14) BitDepth8:\t\t%s\n",getElemValue(ImageOptions,"BitDepth8") );
 
 		/*Print BitDepth16*/
-		printf("\n"); /*make it pretty*/
 		printf("15) BitDepth16:\t\t%s\n",getElemValue(ImageOptions,"BitDepth16") );
 
 		printf("\n\n");
@@ -537,21 +510,21 @@ menu_items_t printmenuImageOptions(mxml_node_t *tree)
         else 
         {
                 /*No ImageOptions node, print empty*/
-                printf("01) AltText: \t\t<n/a>\n\n"
-                        "02) Images: \t\t<n/a>\n\n"
-                        "03) ResizeLargeImages: \t<n/a>\n\n"
-                        "04) MaximumWidth: \t<n/a>\n\n"
-			"05) MaximumHeight: \t<n/a>\n\n"
-			"06) ImproveContrast: \t<n/a>\n\n"
-			"07) Dither: \t\t<n/a>\n\n"
-			"08) Compress: \t\t<n/a>\n\n"
-			"09) LossyLevel: \t<n/a>\n\n"
-			"10) LossyQuality: \t<n/a>\n\n"
-			"11) BitDepth1: \t\t<n/a>\n\n"
-			"12) BitDepth2: \t\t<n/a>\n\n"
-			"13) BitDepth4: \t\t<n/a>\n\n"
-			"14) BitDepth8: \t\t<n/a>\n\n"
-			"15) BitDepth16: \t<n/a>\n\n"
+                printf("01) AltText: \t\t<n/a>\n"
+                        "02) Images: \t\t<n/a>\n"
+                        "03) ResizeLargeImages: \t<n/a>\n"
+                        "04) MaximumWidth: \t<n/a>\n"
+			"05) MaximumHeight: \t<n/a>\n"
+			"06) ImproveContrast: \t<n/a>\n"
+			"07) Dither: \t\t<n/a>\n"
+			"08) Compress: \t\t<n/a>\n"
+			"09) LossyLevel: \t<n/a>\n"
+			"10) LossyQuality: \t<n/a>\n"
+			"11) BitDepth1: \t\t<n/a>\n"
+			"12) BitDepth2: \t\t<n/a>\n"
+			"13) BitDepth4: \t\t<n/a>\n"
+			"14) BitDepth8: \t\t<n/a>\n"
+			"15) BitDepth16: \t<n/a>\n"
                         "\n\n");
         }
 
@@ -575,36 +548,28 @@ menu_items_t printmenuTableOptions(mxml_node_t *tree)
 	{
 
 		/*Print IgnoreTables*/
-		printf("\n"); /*make it pretty*/
-		printf("1) IgnoreTables:\t\t%s\n",getElemValue(TableOptions,"IgnoreTables") );
+		printf("01) IgnoreTables:\t\t%s\n",getElemValue(TableOptions,"IgnoreTables") );
 
 		/*Print AddSeparators*/
-		printf("\n"); /*make it pretty*/
-		printf("2) AddSeparators:\t\t%s\n",getElemValue(TableOptions,"AddSeparators") );
+		printf("02) AddSeparators:\t\t%s\n",getElemValue(TableOptions,"AddSeparators") );
 
 		/*Print UseMinimumDepth*/
-		printf("\n"); /*make it pretty*/
-		printf("3) UseMinimumDepth:\t\t%s\n",getElemValue(TableOptions,"UseMinimumDepth") );
+		printf("03) UseMinimumDepth:\t\t%s\n",getElemValue(TableOptions,"UseMinimumDepth") );
 
 		/*Print MinimumDepth*/
-		printf("\n"); /*make it pretty*/
-		printf("4) MinimumDepth:\t\t%s\n",getElemValue(TableOptions,"MinimumDepth") );
+		printf("04) MinimumDepth:\t\t%s\n",getElemValue(TableOptions,"MinimumDepth") );
 
 		/*Print UseMaximumBottomReach*/
-		printf("\n"); /*make it pretty*/
-		printf("5) UseMaximumBottomReach:\t%s\n",getElemValue(TableOptions,"UseMaximumBottomReach") );
+		printf("05) UseMaximumBottomReach:\t%s\n",getElemValue(TableOptions,"UseMaximumBottomReach") );
 
 		/*Print MaximumBottomReach*/
-		printf("\n"); /*make it pretty*/
-		printf("6) MaximumBottomReach:\t\t%s\n",getElemValue(TableOptions,"MaximumBottomReach") );
+		printf("06) MaximumBottomReach:\t\t%s\n",getElemValue(TableOptions,"MaximumBottomReach") );
 
 		/*Print UnfoldFullPageTables*/
-		printf("\n"); /*make it pretty*/
-		printf("7) UnfoldFullPageTables:\t%s\n",getElemValue(TableOptions,"UnfoldFullPageTables") );
+		printf("07) UnfoldFullPageTables:\t%s\n",getElemValue(TableOptions,"UnfoldFullPageTables") );
 
 		/*Print IgnorePixelWidths*/
-		printf("\n"); /*make it pretty*/
-		printf("8) IgnorePixelWidths:\t\t%s\n",getElemValue(TableOptions,"IgnorePixelWidths") );
+		printf("08) IgnorePixelWidths:\t\t%s\n",getElemValue(TableOptions,"IgnorePixelWidths") );
 
 
 		printf("\n\n");
@@ -613,14 +578,14 @@ menu_items_t printmenuTableOptions(mxml_node_t *tree)
 	else 
 	{
 		/*No TableOptions node, print empty*/
-		printf("1) IgnoreTables: \t\t<n/a>\n\n"
-			"2) AddSeparators: \t\t<n/a>\n\n"
-			"3) UseMinimumDepth: \t\t<n/a>\n\n"
-			"4) MinimumDepth: \t\t<n/a>\n\n"
-			"5) UseMaximumBottomReach: \t<n/a>\n\n"
-			"6) MaximumBottomReach: \t\t<n/a>\n\n"
-			"7) UnfoldFullPageTables: \t<n/a>\n\n"
-			"8) IgnorePixelWidths: \t\t<n/a>\n\n"
+		printf("01) IgnoreTables: \t\t<n/a>\n"
+			"02) AddSeparators: \t\t<n/a>\n"
+			"03) UseMinimumDepth: \t\t<n/a>\n"
+			"04) MinimumDepth: \t\t<n/a>\n"
+			"05) UseMaximumBottomReach: \t<n/a>\n"
+			"06) MaximumBottomReach: \t\t<n/a>\n"
+			"07) UnfoldFullPageTables: \t<n/a>\n"
+			"08) IgnorePixelWidths: \t\t<n/a>\n"
 			"\n\n");
 	}
 
@@ -644,12 +609,10 @@ menu_items_t printmenuColorOptions(mxml_node_t *tree)
 	{
 	
 		/*Print BackgroundColors*/
-		/*printf("\n"); *make it pretty*/
-		printf("1) BackgroundColors:\t%s\n",getElemValue(ColorOptions,"BackgroundColors") );
+		printf("01) BackgroundColors:\t%s\n",getElemValue(ColorOptions,"BackgroundColors") );
 	
 		/*Print TextColors*/
-		printf("\n"); /*make it pretty*/
-		printf("2) TextColors:\t\t%s\n",getElemValue(ColorOptions,"TextColors") );
+		printf("02) TextColors:\t\t%s\n",getElemValue(ColorOptions,"TextColors") );
 
 		printf("\n\n");
 
@@ -657,8 +620,8 @@ menu_items_t printmenuColorOptions(mxml_node_t *tree)
 	else 
 	{
 		/*No ColorOptions node, print empty*/
-		printf("1) BackgroundColors: \t<n/a>\n\n"
-			"2) TextColors: \t\t<n/a>\n\n"
+		printf("01) BackgroundColors: \t<n/a>\n"
+			"02) TextColors: \t\t<n/a>\n"
 			"\n\n");
 	}
 
@@ -681,12 +644,10 @@ menu_items_t printmenuMarginOptions(mxml_node_t *tree)
 	{
 	
 		/*Print LeftRightMargins*/
-		/*printf("\n"); *make it pretty*/
-		printf("1) LeftRightMargins:\t%s\n",getElemValue(MarginOptions,"LeftRightMargins") );
+		printf("01) LeftRightMargins:\t%s\n",getElemValue(MarginOptions,"LeftRightMargins") );
 	
 		/*Print LeftRightPadding*/
-		printf("\n"); /*make it pretty*/
-		printf("2) LeftRightPadding:\t%s\n",getElemValue(MarginOptions,"LeftRightPadding") );
+		printf("02) LeftRightPadding:\t%s\n",getElemValue(MarginOptions,"LeftRightPadding") );
 
 		printf("\n\n");
 
@@ -694,8 +655,8 @@ menu_items_t printmenuMarginOptions(mxml_node_t *tree)
 	else 
 	{
 		/*No MarginOptions node, print empty*/
-		printf("1) LeftRightMargins: \t<n/a>\n\n"
-			"2) LeftRightPadding:\t<n/a>\n\n"
+		printf("01) LeftRightMargins: \t<n/a>\n"
+			"02) LeftRightPadding:\t<n/a>\n"
 			"\n\n");
 	}
 	
@@ -722,24 +683,19 @@ menu_items_t printmenuSecurityOptions(mxml_node_t *tree)
 	{
 
 		/*Print LeftRightMargins*/
-		/*printf("\n"); *make it pretty*/
-		printf("1) Convert:\t\t%s\n",getElemValue(SecurityOptions,"Convert") );
+		printf("01) Convert:\t\t%s\n",getElemValue(SecurityOptions,"Convert") );
 
 		/*Print CopyBeam*/
-		printf("\n"); /*make it pretty*/
-		printf("2) CopyBeam:\t\t%s\n",getElemValue(SecurityOptions,"CopyBeam") );
+		printf("02) CopyBeam:\t\t%s\n",getElemValue(SecurityOptions,"CopyBeam") );
 
 		/*Print CopyAndPaste*/
-		printf("\n"); /*make it pretty*/
-		printf("3) CopyAndPaste:\t%s\n",getElemValue(SecurityOptions,"CopyAndPaste") );
+		printf("03) CopyAndPaste:\t%s\n",getElemValue(SecurityOptions,"CopyAndPaste") );
 
 		/*Print Modify*/
-		printf("\n"); /*make it pretty*/
-		printf("4) Modify:\t\t%s\n",getElemValue(SecurityOptions,"Modify") );
+		printf("04) Modify:\t\t%s\n",getElemValue(SecurityOptions,"Modify") );
 
 		/*Print Print*/
-		printf("\n"); /*make it pretty*/
-		printf("5) Print:\t\t%s\n",getElemValue(SecurityOptions,"Print") );
+		printf("05) Print:\t\t%s\n",getElemValue(SecurityOptions,"Print") );
 		
 		/*Print sub node Expiration*/
 		printf("\nExpiration: \n");
@@ -752,11 +708,10 @@ menu_items_t printmenuSecurityOptions(mxml_node_t *tree)
 		{
 			/*print the subnodes*/
 			/*Print Expires*/
-			printf("4) Expires:\t\t%s\n",getElemValue(SecExpirationOptions,"Expires") );
+			printf("\t 06) Expires:\t\t%s\n",getElemValue(SecExpirationOptions,"Expires") );
 
 			/*Print Date*/
-			printf("\n"); /*make it pretty*/
-			printf("4) Date:\t\t%s\n",getElemValue(SecExpirationOptions,"Date") );
+			printf("\t 07) Date:\t\t%s\n",getElemValue(SecExpirationOptions,"Date") );
 
 		}
 		else
@@ -766,7 +721,8 @@ menu_items_t printmenuSecurityOptions(mxml_node_t *tree)
 				"\t 07) Date:\t\t<n/a>\n");
 
 		}
-
+		/*Print sub node Password*/
+		printf("\nPassword:\n");
 
 	} /*end of the SecurityOptions procesing*/
 	else 
