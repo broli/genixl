bbf0e371e4b704bf37007de42a47d4c9d9699b8c
broli <carlos.ferrabone@gmail.com>
2010-02-10 12:38:22 -0300

    Skeleton of user input

--- src/modules/genixllib-input.c
+++ src/modules/genixllib-input.c
@@ -29,3 +29,46 @@
  *  while in the submenu,MENU_ERROR if it cant allocate or modify the tree, the submenu otherwise*/
 menu_items_t processUserinput(menu_items_t location,mxml_node_t *tree);
 
+/* Helper functions to distribute the work */
+menu_items_t read_user_choice_Main(void);
+
+
+menu_items_t processUserinput(menu_items_t location,mxml_node_t *tree)
+{
+	menu_items_t choice;
+
+	if ( location == MENU_MAIN )
+	{
+		/* we are located in the main menu */
+		/* I should read the user input, 
+		 * and return the corresponding submenu */
+		choice = read_user_choice_Main();
+		return choice;
+	}
+	else
+	{
+		/* we are in a sub menu */
+		/* i should read the user selection (node name)
+		 * and ask about the data (node contents)
+		 * validate, and add to the tree */
+	}
+
+}
+
+
+menu_items_t read_user_choice_Main(void)
+{
+	char *line=NULL;
+	size_t cant=0;   /* we let getline manage this) */
+	
+	if ( getline ( &line, &cant,stdin) == -1 )
+	{
+		fprintf (stderr, "Cant read from stdin\n");
+	}
+	else
+	{
+		/* was able to read user imput
+		 * now i need to sanitize it and 
+		 * make sure it makes some sense */
+	}
+}
